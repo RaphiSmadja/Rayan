e5e694dd38bcf120357888b6d67dd5517abc26df
RaphiSmadja <raphael_smadja@hotmail.com>
2023-10-19 20:32:19 +0200

    continue exercices

--- exercices/src/td/PierrePapierCiseau.java
+++ exercices/src/td/PierrePapierCiseau.java
@@ -1,6 +1,5 @@
 package td;
 
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,14 +7,14 @@ public class PierrePapierCiseau {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        String[] pfc = {"pierre", "feuille", "ciseau" };
+        String[] pfc = {"pierre", "feuille", "ciseau"};
         int compteurUser = 0;
         int compteurOrdi = 0;
         do {
             System.out.println("Saisir Pierre, Feuille ou Ciseau: ");
             String choix = scanner.next();
-            choix= choix.toLowerCase();
-            int choixordi = random.nextInt(0,3);
+            choix = choix.toLowerCase();
+            int choixordi = random.nextInt(0, 3);
             System.out.println(pfc[choixordi]);
             if (pfc[choixordi].equals(choix)) {
                 System.out.println("Egalité! Recommencez.");
@@ -25,7 +24,7 @@ public class PierrePapierCiseau {
             } else if (pfc[choixordi].equals("feuille") && choix.equals("ciseau")) {
                 System.out.println("Vous avez gagner !");
                 compteurUser++;
-            } else if (pfc[choixordi].equals("ciseau") && choix.equals("pierre")){
+            } else if (pfc[choixordi].equals("ciseau") && choix.equals("pierre")) {
                 System.out.println("Vous avez gagner !");
                 compteurUser++;
             } else if (choix.equals("pierre") && pfc[choixordi].equals("feuille")
@@ -34,7 +33,7 @@ public class PierrePapierCiseau {
                 System.out.println("Vous avez Perdu !");
                 compteurOrdi++;
             }
-        }while(compteurOrdi<3 && compteurUser<3);
-        System.out.println("L'ordinateur a un compteur de: "+compteurOrdi+ " et vous avez un compteur de: "+compteurUser);
+        } while (compteurOrdi < 3 && compteurUser < 3);
+        System.out.println("L'ordinateur a un compteur de: " + compteurOrdi + " et vous avez un compteur de: " + compteurUser);
     }
 }
